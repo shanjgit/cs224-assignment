ce08f0b1ef399aad41eeaa7c265767a896864de3
shanjgit <lightemit@gmail.com>
2019-02-22 00:16:54 +0800

    upload assignment1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# cs224-assignment
+# Stanford CS224D/CS224N: Natural Language Processing with Deep Learning
+
+Notes
+-----
+* The assignment 1 is based on [CS224d Spring2016](http://cs224d.stanford.edu/). I wrote this assignment in `Python3.7` environment and bumped into encoding issues when running q4_sentiment.py and q4_softmaxreg.py. I struggled for a while and finally gave, deciding to move on to cs224n, the newest version of this course, for the rest assignment.
+
+* Handout written by latex for the assinment 1 will be uploaded later. 
---
+++ assignment1/q2_sigmoid.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    Compute the sigmoid function for the input here.
+    """
+    
+    ### YOUR CODE HERE
+    ans = 1/(1 + np.exp(-x))
+
+    ### END YOUR CODE
+    
+    return ans
+
+
+def sigmoid_grad(f):
+    """
+    Compute the gradient for the sigmoid function here. Note that
+    for this implementation, the input f should be the sigmoid
+    function value of your original input x. 
+    """
+    
+    ### YOUR CODE HERE
+    ans = f * (1-f)
+
+    ### END YOUR CODE
+    
+    return ans
+
+
+def test_sigmoid_basic():
+    """
+    Some simple tests to get you started. 
+    Warning: these are not exhaustive.
+    """
+    print("Running basic tests...")
+    x = np.array([[1, 2], [-1, -2]])
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print(f)
+    assert np.amax(f - np.array([[0.73105858, 0.88079708], 
+        [0.26894142, 0.11920292]])) <= 1e-6
+    print(g)
+    assert np.amax(g - np.array([[0.19661193, 0.10499359], [0.19661193, 0.10499359]])) <= 1e-6
+    print("You should verify these results!\n")
+
+
+def test_sigmoid(): 
+    """
+    Use this space to test your sigmoid implementation by running:
+        python q2_sigmoid.py 
+    This function will not be called by the autograder, nor will
+    your tests be graded.
+    """
+    print("Running your tests...")
+    ### YOUR CODE HERE
+    x = np.array([0])
+    f = sigmoid(x)
+    g =sigmoid_grad(f)
+    assert np.amax(f - np.array([1/2])) <= 1e-6
+    assert np.amax(g-np.array([1/4])) <= 1e-6
+    assert f.ndim == 1
+    assert g.ndim == 1
+    ### END YOUR CODE
+    print("pass test!")
+
+
+if __name__ == "__main__":
+    test_sigmoid_basic()
+    test_sigmoid()
